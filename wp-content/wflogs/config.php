32f2f1234f6d417203f83b8cc7c82e51972ef8cb
Votre Nomcd <votre@email.com>
2026-05-26 16:03:13 +0000

    Mon Projet cms

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"-myjf}qxvu$[REGvL3(H[14Iz.YYmT85$Ws.{vDd~2`u.H$uu4vu$~wzF(PONu=B";s:7:"version";s:5:"1.1.0";}
